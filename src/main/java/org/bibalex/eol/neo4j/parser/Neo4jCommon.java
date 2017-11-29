38ad85933601da9b627b3938160f654edca45117
ba <ba@CDLIN-HICT8169.local.bibalex.dom>
2017-11-29 10:56:30 +0200

    Adding Common

--- src/main/java/org/bibalex/eol/neo4j/parser/Neo4jCommon.java
+++ src/main/java/org/bibalex/eol/neo4j/parser/Neo4jCommon.java
@@ -4,17 +4,44 @@ import org.neo4j.driver.v1.*;
 
 import static org.neo4j.driver.v1.Values.parameters;
 
-import java.util.logging.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 public class Neo4jCommon {
     Session session;
-    public static int autoId = 1;
-    Logger logger =  Logger.getLogger("NEO4J_COMMON");
+    int autoId = 0;
+    Logger logger =  LoggerFactory.getLogger(Neo4jCommon.class);
+
+    public int getAutoId()
+    {
+        String query = "MATCH (n) RETURN n.generated_auto_id ORDER BY n.generated_auto_id DESC LIMIT 1";
+        StatementResult result = getSession().run(query);
+
+        if (result.hasNext())
+        {
+            logger.debug("AutoId found ");
+            Record record = result.next();
+            autoId = record.get("n.generated_auto_id").asInt() + 1;
+
+        }
+        else
+        {
+            logger.debug("Autoid IS 1");
+            autoId =  1;
+        }
+
+        return autoId;
+
+    }
 
     public Session getSession (){
         if (session == null || !session.isOpen()) {
             Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "neo4j"));
             session = driver.session();
+
         }
         return session;
     }
@@ -22,15 +49,15 @@ public class Neo4jCommon {
 
     public boolean createRelationBetweenNodeAndSynonyms(int acceptedNodeGeneratedId, int synonymGeneratedNodeId)
     {
-        logger.info("Creating synonym relation between acceptedNode" + acceptedNodeGeneratedId + "and synonym " + synonymGeneratedNodeId);
+        logger.debug("Creating synonym relation between acceptedNode" + acceptedNodeGeneratedId + "and synonym " + synonymGeneratedNodeId);
         String query = " MATCH(s:Synonym), (a:Node) WHERE s.generated_auto_id = {synonymGeneratedNodeId} AND " +
                 "a.generated_auto_id = {acceptedNodeGeneratedId} CREATE (s)-[r:IS_SYNONYM_OF]->(a) RETURN r";
         StatementResult result = getSession().run(query, parameters("synonymGeneratedNodeId",
                 synonymGeneratedNodeId, "acceptedNodeGeneratedId", acceptedNodeGeneratedId));
         if (result.hasNext())
         {
-            logger.info("Node created");
-            logger.info("Synonym Accepted relation created");
+            logger.debug("Node created");
+            logger.debug("Synonym Accepted relation created");
             return true;
         }
         return false;
@@ -38,63 +65,87 @@ public class Neo4jCommon {
 
     public int getNodeIfExist(String nodeId, int resourceId)
     {
-        logger.info("Searching for node with nodeId" + nodeId + "in resource with resourceId" + resourceId);
+        logger.debug("Searching for node with nodeId" + nodeId + "in resource with resourceId" + resourceId);
         String query = "MATCH (n) WHERE n.node_id = {nodeId} AND n.resource_id = {resourceId} " +
                 " RETURN n.generated_auto_id";
         StatementResult result = getSession().run(query, parameters("nodeId",nodeId, "resourceId",  resourceId ));
-        if (!result.hasNext()) {return -1;}
+        if (result.hasNext())
+        {
+            Record record = result.next();
+            logger.debug("THe result of search" +record.get("n.generated_auto_id").asInt() );
+            return record.get("n.generated_auto_id").asInt();
+        }
 
-        Record record = result.next();
-        return record.get("n.generated_auto_id").asInt();
+        else
+            {
+            logger.debug("result is -1");
+            return -1;
+        }
 
     }
 
     public int getAcceptedNodeIfExist(String nodeId, String scientificName, int resourceId)
     {
-        logger.info("Searching for accepted node with nodeId" + nodeId + "in resource with resourceId" + resourceId);
+        logger.debug("Searching for accepted node with nodeId" + nodeId + "in resource with resourceId" + resourceId);
         String query = "MATCH (n:Node) WHERE n.node_id = {nodeId} AND n.resource_id = {resourceId} AND " +
                 "n.scientific_name = {scientificName} RETURN n.generated_auto_id";
-        StatementResult result = getSession().run(query,parameters("nodeId", nodeId, "resourceId",resourceId));
-        if (!result.hasNext()) {return -1;}
+        StatementResult result = getSession().run(query,parameters("nodeId", nodeId,  "resourceId", resourceId,
+                "scientificName",scientificName));
 
-        Record record = result.next();
-        return record.get("n.generated_auto_id").asInt();
+        if (result.hasNext())
+        {
+            Record record = result.next();
+            logger.debug("THe result of search" +record.get("n.generated_auto_id").asInt() );
+            return record.get("n.generated_auto_id").asInt();
+        }
+
+        else
+        {
+            logger.debug("result is -1");
+            return -1;
+        }
 
     }
 
     public int createAcceptedNode(int resourceId, String nodeId, String scientificName, String rank,
                                   int parentGeneratedNodeId)
     {
+        autoId = getAutoId();
         String create_query = "CREATE (n:Node {resource_id: {resourceId}, node_id: {nodeId}," +
                 " scientific_name: {scientificName}, rank: {rank}, generated_auto_id: {autoId}})" +
                 "RETURN n.generated_auto_id";
         StatementResult result = getSession().run(create_query, parameters( "resourceId", resourceId,
                 "nodeId", nodeId, "scientificName", scientificName, "rank", rank, "autoId", autoId ));
-        if (parentGeneratedNodeId != 0) {createChildParentRelation(parentGeneratedNodeId, autoId);}
+        if (parentGeneratedNodeId != 0) {
+            logger.debug("Parent avaliable with id " + parentGeneratedNodeId);
+            createChildParentRelation(parentGeneratedNodeId, autoId);
+        }
         autoId ++;
         Record record = result.next();
         return record.get("n.generated_auto_id").asInt();
     }
 
-    public boolean createChildParentRelation(int parentGeneratedNodeId, int childGeneratedNodeId)
+    public void createChildParentRelation(int parentGeneratedNodeId, int childGeneratedNodeId)
     {
-        System.out.println("I am creating relations");
+
         String query = " MATCH(p:Node), (c:Node) WHERE p.generated_auto_id = {parentGeneratedNodeId} AND " +
                 "c.generated_auto_id = {childGeneratedNodeId} CREATE (p)-[r:IS_PARENT_OF]->(c) RETURN r";
         StatementResult result = getSession().run(query, parameters("parentGeneratedNodeId",
                 parentGeneratedNodeId, "childGeneratedNodeId", childGeneratedNodeId));
         if (result.hasNext())
         {
-            logger.info("Node" + childGeneratedNodeId+"created");
-            logger.info("Child Parent relation created");
-            return true;
+            logger.debug("Node" + childGeneratedNodeId+"created");
+            logger.debug("Child Parent relation created with parentgeneratedNodeId " + parentGeneratedNodeId);
+
         }
-        return false;
+        else
+         logger.debug("Child Parent relation not created for child "+ childGeneratedNodeId + "and parent "+ parentGeneratedNodeId);
     }
 
     public int createSynonymNode(int resourceId, String nodeId, String scientificName, String rank,
                                  String acceptedNodeId, int acceptedNodeGeneratedId)
     {
+        autoId = getAutoId();
         String create_query = "CREATE (s:Synonym {resource_id: {resourceId}, node_id: {nodeId}," +
                 "scientific_name: {scientificName}, rank: {rank}, generated_auto_id: {autoId}})" +
                 " RETURN s.generated_auto_id";
@@ -104,7 +155,7 @@ public class Neo4jCommon {
         boolean relation = createRelationBetweenNodeAndSynonyms(acceptedNodeGeneratedId,
                 record.get("s.generated_auto_id").asInt());
         autoId++;
-        if (relation) {logger.info("Synonym created");}
+        if (relation) {logger.debug("Synonym created");}
         return record.get("s.generated_auto_id").asInt();
     }
 
@@ -115,7 +166,18 @@ public class Neo4jCommon {
                 " scientific_name: {scientificName}})<-[r:IS_SYNONYM_OF]-(s:Synonym) RETURN DISTINCT s.generated_auto_id";
         StatementResult result = getSession().run(query, parameters("nodeId", nodeId,"resourceId",resourceId,
                 "scientificName",scientificName));
-        Record record = result.next();
-        return record.get("s.generated_auto_id").asInt();
+        if (result.hasNext())
+        {
+            Record record = result.next();
+            logger.debug("THe result of search Synoym" +record.get("s.generated_auto_id").asInt() );
+            return record.get("s.generated_auto_id").asInt();
+        }
+
+        else
+        {
+            logger.debug("result is -1");
+            return -1;
+        }
+
     }
 }
